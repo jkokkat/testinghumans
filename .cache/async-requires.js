32857bb94878108a6adf00427d99cae6e25b5f1a
Jasmine Kokkat <jazzimkokkat@gmail.com>
2021-11-10 15:47:57 -0500

    added redirect

---
+++ .cache/async-requires.js
@@ -0,0 +1,20 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-about-page-about-page-js": () => import("./../../../src/pages/about-page/AboutPage.js" /* webpackChunkName: "component---src-pages-about-page-about-page-js" */),
+  "component---src-pages-components-footer-footer-js": () => import("./../../../src/pages/components/Footer/Footer.js" /* webpackChunkName: "component---src-pages-components-footer-footer-js" */),
+  "component---src-pages-components-header-header-js": () => import("./../../../src/pages/components/Header/Header.js" /* webpackChunkName: "component---src-pages-components-header-header-js" */),
+  "component---src-pages-components-submission-form-submission-form-js": () => import("./../../../src/pages/components/submission-form/submission-form.js" /* webpackChunkName: "component---src-pages-components-submission-form-submission-form-js" */),
+  "component---src-pages-contact-us-contact-us-js": () => import("./../../../src/pages/contact-us/contact-us.js" /* webpackChunkName: "component---src-pages-contact-us-contact-us-js" */),
+  "component---src-pages-feature-page-currently-featured-js": () => import("./../../../src/pages/feature-page/currently-featured.js" /* webpackChunkName: "component---src-pages-feature-page-currently-featured-js" */),
+  "component---src-pages-feature-page-feature-page-js": () => import("./../../../src/pages/feature-page/feature-page.js" /* webpackChunkName: "component---src-pages-feature-page-feature-page-js" */),
+  "component---src-pages-home-1-home-1-js": () => import("./../../../src/pages/home1/home1.js" /* webpackChunkName: "component---src-pages-home-1-home-1-js" */),
+  "component---src-pages-home-2-home-2-js": () => import("./../../../src/pages/home2/home2.js" /* webpackChunkName: "component---src-pages-home-2-home-2-js" */),
+  "component---src-pages-home-3-home-3-js": () => import("./../../../src/pages/home3/home3.js" /* webpackChunkName: "component---src-pages-home-3-home-3-js" */),
+  "component---src-pages-home-4-home-4-js": () => import("./../../../src/pages/home4/home4.js" /* webpackChunkName: "component---src-pages-home-4-home-4-js" */),
+  "component---src-pages-home-home-js": () => import("./../../../src/pages/home/Home.js" /* webpackChunkName: "component---src-pages-home-home-js" */),
+  "component---src-pages-nominate-page-nominate-js": () => import("./../../../src/pages/nominate-page/nominate.js" /* webpackChunkName: "component---src-pages-nominate-page-nominate-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,22 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/.cache/dev-404-page.js")),
+  "component---src-pages-about-page-about-page-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/about-page/AboutPage.js")),
+  "component---src-pages-components-footer-footer-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/components/Footer/Footer.js")),
+  "component---src-pages-components-header-header-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/components/Header/Header.js")),
+  "component---src-pages-components-submission-form-submission-form-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/components/submission-form/submission-form.js")),
+  "component---src-pages-contact-us-contact-us-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/contact-us/contact-us.js")),
+  "component---src-pages-feature-page-currently-featured-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/feature-page/currently-featured.js")),
+  "component---src-pages-feature-page-feature-page-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/feature-page/feature-page.js")),
+  "component---src-pages-home-1-home-1-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/home1/home1.js")),
+  "component---src-pages-home-2-home-2-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/home2/home2.js")),
+  "component---src-pages-home-3-home-3-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/home3/home3.js")),
+  "component---src-pages-home-4-home-4-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/home4/home4.js")),
+  "component---src-pages-home-home-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/home/Home.js")),
+  "component---src-pages-nominate-page-nominate-js": preferDefault(require("/Users/jasminekokkat/Downloads/humans-proj/src/pages/nominate-page/nominate.js"))
+}
+
