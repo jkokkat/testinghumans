85df694c6c83072f6023db3f6d8d889df08e6249
Jasmine Kokkat <jazzimkokkat@gmail.com>
2021-12-01 00:47:56 -0500

    fixing form name

--- src/pages/components/submission-form/submission-form.js
+++ src/pages/components/submission-form/submission-form.js
@@ -99,7 +99,7 @@ export default class SubmissionForm extends Component {
             <div id="submission">
                 <p id="intro">{this.props.intro}</p>
 
-                <form name="form" method="post" id="text-fields" onSubmit={this.handleSubmit} data-netlify="true" netlify-honeypot="bot-field">
+                <form name="form-name" method="post" id="text-fields" onSubmit={this.handleSubmit} data-netlify="true" netlify-honeypot="bot-field">
                     <input type="hidden" name="bot-field" />
                     <input type="hidden" name="form-name" value="form"/>
                     <p hidden>
